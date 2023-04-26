f0f32e1d0e777500c0ffc679d7c79257226b42e4
David Thomas <dthmasp@amazon.com>
2023-04-26 16:36:27 +0000

    Add python debugging config

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -20,7 +20,29 @@ return {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
-      -- ensure_installed = { "python" },
+      ensure_installed = { "python" },
+      handlers = {
+        python = function(source_name)
+          local dap = require "dap"
+          dap.adapters.python = {
+            type = "executable",
+            command = "/usr/bin/python3",
+            args = {
+              "-m",
+              "debugpy.adapter",
+            },
+          }
+
+          dap.configurations.python = {
+            {
+              type = "python",
+              request = "launch",
+              name = "Launch file",
+              program = "${file}",   -- This configuration will launch the current file if used.
+            },
+          }
+        end,
+      },
     },
   },
 }
